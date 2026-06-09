517d3ceab81f71607555a5370fa943ab6706b1f7
Dastan_27 <maikleo2019@gmail.com>
2026-06-09 16:26:21 -0500

    Ultima

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -12,11 +12,8 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
-      ],
-      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
       "no-unused-expressions": "error",
       "no-empty": "off",
       "no-unsafe-finally": "off",
